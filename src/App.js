dfaf8ef608524f439072dfd220164c59b0252ac6
Mariana de Paula <99264876+mariana2903@users.noreply.github.com>
2024-05-14 10:31:57 -0300

    Aplicação React - início

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Inteli - Instituto de Tecnologia e Liderança</h1>
+      <p>Já se fazem faculdades como futuramente.</p>
     </div>
   );
 }
